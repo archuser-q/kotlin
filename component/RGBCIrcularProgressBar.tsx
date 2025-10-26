8aef2ab131a6702e00e7c1bd67097c2737351ed8
archuser-q <availableemailaddress123@tutamail.com>
2025-10-26 23:13:09 +0700

    adding progress bar for UV

---
+++ component/RGBCIrcularProgressBar.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Text } from 'react-native';
+import { AnimatedCircularProgress } from "react-native-circular-progress";
+
+export default function RGBCircularProgressBar() {
+  const [color, setColor] = useState("#22c55e");
+  const getColorFromFill = (fill: number) => {
+    if (fill < 20) return "#22c55e";   
+    if (fill < 40) return "#eab308";   
+    if (fill < 60) return "#f97316";   
+    if (fill < 80) return "#ef4444";   
+    return "#8b5cf6";                  
+  };
+  return (
+    <AnimatedCircularProgress
+      size={70}
+      width={6}
+      fill={50}
+      tintColor={color}
+      backgroundColor="#d1d5db"
+      rotation={225}
+      arcSweepAngle={270}
+      lineCap="round"
+    >
+      {(fill: number) => {
+        const newColor = getColorFromFill(fill);
+        if (newColor !== color) setColor(newColor); 
+        return (
+          <Text
+            className="text-2xl font-extrabold"
+            style={{ color: newColor }}
+          >
+            {Math.round(fill)}
+          </Text>
+        );
+      }}
+    </AnimatedCircularProgress>
+  );
+}
