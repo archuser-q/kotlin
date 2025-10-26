588d8cb528ce5eabf65643c4a85629998f856cff
archuser-q <availableemailaddress123@tutamail.com>
2025-10-26 16:07:40 +0700

    2/3 main screen completed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,34 @@
-import { Stack } from "expo-router";
-import "./global.css";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#2563eb", // màu xanh active
+        tabBarInactiveTintColor: "gray",   // màu icon không active
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="addedLocation"
+        options={{
+          title: "Added Location",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import Ionicons from "@expo/vector-icons/build/Ionicons";
+import React from "react";
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView className="bg-white">
+      <View className="pt-12 pl-5 pb-10">
+        <TouchableOpacity
+          onPress={() => console.log("Go back")}
+        >
+          <Ionicons name="arrow-back" size={30} color="black" />
+        </TouchableOpacity>
+      </View>
+      <Text className="text-4xl pl-7 pb-3 leading-relaxed">Managing cities</Text>
+      <View className="flex-row items-center bg-gray-100 rounded-3xl mx-4 px-4">
+        <Ionicons name="search-outline" size={20} color="#9ca3af" />
+        <TextInput
+          placeholder="Enter location ..."
+          placeholderTextColor="#9ca3af"
+          className="flex-1 ml-2 text-gray-800 text-base"
+        />
+      </View>
+      <View 
+        className="pl-7 pt-4"
+      >
+        <Text 
+          className="text-gray-400"
+        >
+          Added location
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
