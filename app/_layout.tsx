9f6a191621210d3254dd8545879991d4436b3d97
archuser-q <availableemailaddress123@tutamail.com>
2025-10-27 22:09:24 +0700

    update route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,52 +1,19 @@
-import { Ionicons } from "@expo/vector-icons";
-import { Tabs } from "expo-router";
+import { Stack } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
   return (
-    <Tabs
+    <Stack
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: "#2563eb", // màu xanh active
-        tabBarInactiveTintColor: "gray",   // màu icon không active
       }}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home-outline" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="addedLocation"
-        options={{
-          title: "Added Location",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home-outline" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="fullLocationInfo"
-        options={{
-          title: "Location Info",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home-outline" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="testLayout"
-        options={{
-          title: "LineChart",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home-outline" size={size} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      <Stack.Screen name="index" options={{
+        animation: "fade"
+      }}/>
+      <Stack.Screen name="addedLocation" options={{
+        animation: "fade"
+      }}/>
+    </Stack>
   );
 }
