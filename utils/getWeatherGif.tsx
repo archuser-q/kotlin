5257f98cb5bafe8ae23abe1206e9fc053fac0195
archuser-q <availableemailaddress123@tutamail.com>
2025-10-29 13:10:09 +0700

    update search data and gif logic

--- utils/getWeatherGif.tsx
+++ utils/getWeatherGif.tsx
@@ -15,25 +15,52 @@ export default function getWeatherGif({
   wind_gusts_10m,
   uv_index,
 }: WeatherGifProps) {
-  if (rain > 5) {
-    if (wind_gusts_10m > 10) return require('../assets/storm.gif');
-    return isDay ? require('../assets/rain.gif') : require('../assets/Night_Sky_Rain.gif');
+  const currentHour = new Date().getHours();
+  if (isDay && (currentHour >= 5 && currentHour <= 7)) {
+    return require('../assets/sunrise.gif');
+  }
+  if (isDay && (currentHour >= 17 && currentHour <= 19)) {
+    return require('../assets/sunset.gif');
+  }
+
+  if (rain > 5 && wind_gusts_10m > 20) {
+    return require('../assets/storm.gif');
+  }
+
+  if (rain > 2) {
+    return isDay 
+      ? require('../assets/rain.gif') 
+      : require('../assets/Night_Sky_Rain.gif');
   }
 
   if (visibility < 1000) {
-    return isDay ? require('../assets/fog.gif') : require('../assets/fogNight.gif');
+    return isDay 
+      ? require('../assets/fog.gif') 
+      : require('../assets/fogNight.gif');
   }
 
-  if (cloud_cover > 70) {
-    return isDay ? require('../assets/daytimeWithCloud.gif') : require('../assets/nighttime.gif');
+  if (!isDay) {
+    if (cloud_cover > 50) {
+      return require('../assets/nighttime.gif'); 
+    }
+    return require('../assets/clearSkyNight.gif');
   }
 
-  if (cloud_cover > 30) {
-    return isDay ? require('../assets/afternoonCloud.gif') : require('../assets/nighttime.gif');
-  }
   if (uv_index >= 6) {
     return require('../assets/sunnyday.gif');
   }
 
-  return isDay ? require('../assets/daytime.gif') : require('../assets/clearSkyNight.gif');
+  if (cloud_cover > 70 && currentHour >= 12 && currentHour < 17) {
+    return require('../assets/afternoonCloud.gif');
+  }
+
+  if (cloud_cover > 70) {
+    return require('../assets/daytimeWithCloud.gif');
+  }
+
+  if (cloud_cover > 30 && currentHour >= 12 && currentHour < 17) {
+    return require('../assets/afternoonCloud.gif');
+  }
+
+  return require('../assets/daytime.gif');
 }
