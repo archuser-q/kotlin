5f4e8f32d52feebc8670ad98ac7edab820f4983d
archuser-q <availableemailaddress123@tutamail.com>
2025-10-30 21:08:32 +0700

    update

---
+++ data/serverData.tsx
@@ -0,0 +1,28 @@
+import { useLocalSearchParams } from "expo-router";
+
+export const useData = () => {
+  const { cityName, weatherData, airQualityData } = useLocalSearchParams();
+  const weather = JSON.parse(weatherData as string);
+  const airQuality = JSON.parse(airQualityData as string);
+  const windDirectionPercent = (weather.current.wind_direction_10m / 360) * 100;
+  const now = new Date();
+  const currentHour = now.getHours();
+
+  const data = weather?.hourly?.time && weather?.hourly?.temperature_2m
+    ? weather.hourly.time
+        .map((time: string, index: number) => {
+          const hour = new Date(time).getHours();
+          const isToday = new Date(time).getDate() === now.getDate();
+          const isCurrentHour = isToday && hour === currentHour;
+          return {
+            time: isCurrentHour ? 'Current' : `${hour}h`,
+            temp: Math.round(weather.hourly.temperature_2m[index]),
+            timestamp: new Date(time).getTime()
+          };
+        })
+        .filter(item => item.timestamp >= now.getTime())
+        .slice(0, 12)
+    : [];
+
+  return { cityName, weather, airQuality, windDirectionPercent, data };
+};
