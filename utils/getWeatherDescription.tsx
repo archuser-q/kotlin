95c6aca51077d4e8adc23dad709f5bb775fc2ed9
archuser-q <availableemailaddress123@tutamail.com>
2025-10-31 21:08:37 +0700

    add file

---
+++ utils/getWeatherDescription.tsx
@@ -0,0 +1,28 @@
+export default function getWeatherDescription(code) {
+  const descriptions = {
+    0: "Clear sky",
+    1: "Mainly clear",
+    2: "Partly cloudy",
+    3: "Overcast",
+    45: "Fog",
+    48: "Freezing fog",
+    51: "Light drizzle",
+    53: "Moderate drizzle",
+    55: "Dense drizzle",
+    61: "Light rain",
+    63: "Moderate rain",
+    65: "Heavy rain",
+    71: "Light snow",
+    73: "Moderate snow",
+    75: "Heavy snow",
+    80: "Light rain showers",
+    81: "Moderate rain showers",
+    82: "Violent rain showers",
+    85: "Light snow showers",
+    86: "Heavy snow showers",
+    95: "Thunderstorm",
+    96: "Thunderstorm with light hail",
+    99: "Thunderstorm with heavy hail"
+  };
+  return descriptions[code] || "Unknown weather";
+}
