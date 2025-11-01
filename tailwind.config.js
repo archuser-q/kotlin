836d8dd4fa4ad996975e252df126eee73df17b91
archuser-q <availableemailaddress123@tutamail.com>
2025-11-01 09:32:12 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: ["./App.tsx", 
-    "./components/**/*.{js,jsx,ts,tsx}",
+    "./component/**/*.{js,jsx,ts,tsx}",
   "./app/**/*.{js,jsx,ts,tsx}",],
   presets: [require("nativewind/preset")],
   theme: {
